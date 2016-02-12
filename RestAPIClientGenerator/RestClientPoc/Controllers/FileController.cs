618c463728972722e850e20a9f3e1a107ff2bead
jevsvel <jvelarde@nersoft.com>
2016-02-11 22:25:19 -0700

    Update all libraries and add dotnetzip library.

---
+++ RestAPIClientGenerator/RestClientPoc/Controllers/FileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RestClientPoc.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        [System.Web.Http.HttpGet]
+        public FileResult Download()
+        {
+           return null;
+        }
+    }
+}
---
+++ RestAPIClientGenerator/RestClientPoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestClientPoc.MvcApplication" Language="C#" %>
